b5734260575b1ffdfc0e3ebfe4c9aea5bec69dbe
Xiaoming985 <xiaoming985@163.com>
2023-05-01 00:23:09 +0800

    解决docs工程饮用components组件问题

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,12 @@
+import DefaultTheme from 'vitepress/theme';
+// import '@galaxy-ui/theme-chalk/src/index.scss';
+import GalaxyUI from '@galaxy-ui/components';
+
+export default {
+  ...DefaultTheme,
+  enhanceApp: async ({ app, router, siteData }) => {
+    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
+    // custom router. `siteData`` is a `ref`` of current site-level metadata.
+    app.use(GalaxyUI);
+  }
+};
