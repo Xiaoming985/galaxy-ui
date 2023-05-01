f034db9c5e41228a059222b8ef0a2dcf420db0ea
Xiaoming985 <xiaoming985@163.com>
2023-05-02 04:14:51 +0800

    添加icon图标

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <div>Hello, PNPM + Monorepo + Vite</div>
-    <ga-button type="primary">Button</ga-button>
+    <ga-button>默认按钮</ga-button>
+    <ga-button type="primary">主要按钮</ga-button>
+    <ga-button type="success">成功按钮</ga-button>
+    <ga-button type="info">信息按钮</ga-button>
+    <ga-button type="warning">警告按钮</ga-button>
+    <ga-button type="danger">危险按钮</ga-button>
+    <ga-button type="primary" loading>loading</ga-button>
   </div>
 </template>
 
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import GalaxyUI from "@galaxy-ui/components";
+import "@galaxy-ui/theme-chalk/src/index.scss";
 
 createApp(App).use(GalaxyUI).mount("#app");
